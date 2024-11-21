fe30b40acbcfa3ab187295f22347f934a0c1b837
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-11-21 20:39:09 +0100

    Remove unused code.

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,6 @@
 import {
   calculateCorrectionBitsForFormat,
   calculateCorrectionBitsForVersion,
-  calculateErrorCorrectionBitsForDataEntered,
   showFormatPatternCompletion,
   showHowFormatCorrectionBitsAreCalculated,
   showHowVersionCorrectionBitsAreCalculated,
@@ -12,7 +11,6 @@ import {
   alignmentPatternLightColor,
   dataEnteredDarkColor,
   dataEnteredLightColor,
-  defaultDarkColor,
   defaultLightColor,
   errorCodewordsDarkColor,
   errorCodewordsLightColor,
@@ -79,152 +77,16 @@ function readInputs() {
 
 ELEMENTS.GENERATE_QR_CODE_BUTTON.addEventListener('click', () => {
   const { message, errorCapacity } = readInputs();
-  generateQrCode2(message, errorCapacity);
+  generateQrCode(message, errorCapacity);
 });
-
-function generateQrCode(message, errorCapacity) {
-  const qrCodeVersion = QR_CODE_INFO.find((qr) => qr.errorCapacity[errorCapacity] >= message.length);
-
-  const versionInBinary = (qrCodeVersion.version >>> 0).toString(2).padStart(6, '0');
-  const errorCorrectionBitsVersionPattern = calculateCorrectionBitsForVersion(versionInBinary);
-
-  const ERROR_CAPACITY_CODIFICATION = { L: '01', M: '00', Q: '11', H: '10' };
-  const errorCapacityCodificationBits = ERROR_CAPACITY_CODIFICATION[errorCapacity];
-  const maskCodificationBits = '010'; // TODO calculate the mask code
-  const formatDataInBinary = `${errorCapacityCodificationBits}${maskCodificationBits}`;
-  const formatCorrectionBits = calculateCorrectionBitsForFormat(formatDataInBinary);
-  const formatBitsAfterXor = applyXOR(`${formatDataInBinary}${formatCorrectionBits}`, '101010000010010');
-
-  // Print final qr code version
-  const generatedFinalQrCodeMatrix = createQrCodeMatrix(qrCodeVersion.size);
-  paintModulesForAllPositionPatterns(generatedFinalQrCodeMatrix);
-  paintModulesForAllAlignmentPatterns(generatedFinalQrCodeMatrix);
-  paintModulesForAllTimingPatterns(generatedFinalQrCodeMatrix);
-  paintModulesForAllVersionPatterns(generatedFinalQrCodeMatrix, `${versionInBinary}${errorCorrectionBitsVersionPattern}`);
-  paintModulesForAllFormatPatterns(generatedFinalQrCodeMatrix, formatBitsAfterXor);
-  paintSvgQrCode(CSS_IDS.GENERATED_FINAL_QR_CODE, generatedFinalQrCodeMatrix, { withGrid: false, margin: 0 });
-
-  // Show empty qr code
-  const emptyQrCodeMatrix = createQrCodeMatrix(qrCodeVersion.size);
-  paintSvgQrCode(CSS_IDS.EMPTY_QR_CODE, emptyQrCodeMatrix);
-
-  // Set the number of characters to codify in the qr code
-  document.querySelectorAll(CSS_CLASSES.NUMBER_OF_CHARACTERS_IN_THE_INPUT).forEach((e) => (e.textContent = message.length));
-
-  // Set the selected error capacity
-  document.querySelectorAll(CSS_CLASSES.SELECTED_ERROR_CAPACITY_LEVEL).forEach((e) => (e.textContent = errorCapacity));
-
-  hightlightVersionInVersionsTable(qrCodeVersion.version, errorCapacity);
-  hightlightVersionInAlignmentPatternsTable(qrCodeVersion.version);
-
-  // Show qr code sections
-  const sectionsQrCodeMatrix = createQrCodeMatrix(qrCodeVersion.size);
-  paintModulesForAllPositionPatterns(sectionsQrCodeMatrix, positionPatternDarkColor, positionPatternLightColor);
-  paintModulesForAllAlignmentPatterns(sectionsQrCodeMatrix, alignmentPatternDarkColor, alignmentPatternLightColor);
-  paintModulesForAllTimingPatterns(sectionsQrCodeMatrix, timingPatternDarkColor, timingPatternLightColor);
-  paintModulesForAllVersionPatterns(
-    sectionsQrCodeMatrix,
-    `${versionInBinary}${errorCorrectionBitsVersionPattern}`,
-    versionInformationDarkColor,
-    versionInformationLightColor
-  );
-  paintModulesForAllFormatPatterns(sectionsQrCodeMatrix, formatBitsAfterXor, formatInformationDarkColor, formatInformationLightColor);
-  paintSvgQrCode(CSS_IDS.QR_CODE_SECTIONS, sectionsQrCodeMatrix, { withGrid: true, margin: 3 });
-
-  // Explanation version information
-  Array.from(document.querySelectorAll(CSS_CLASSES.QR_CODE_VERSION)).forEach((e) => (e.textContent = qrCodeVersion.version));
-  Array.from(document.querySelectorAll(CSS_CLASSES.QR_CODE_VERSION_BINARY)).forEach((e) => (e.textContent = versionInBinary));
-  showHowVersionCorrectionBitsAreCalculated(versionInBinary);
-  Array.from(document.querySelectorAll(CSS_CLASSES.QR_CODE_VERSION_BINARY)).forEach((e) => (e.textContent = versionInBinary));
-  Array.from(document.querySelectorAll(CSS_CLASSES.VERSION_CORRECTION_BITS)).forEach(
-    (e) => (e.textContent = errorCorrectionBitsVersionPattern)
-  );
-  showVersionPatternCompletion(`${versionInBinary}${errorCorrectionBitsVersionPattern}`);
-
-  // Explanation format information
-  Array.from(document.querySelectorAll(CSS_CLASSES.ERROR_CAPACITY_CODIFICATION_BITS)).forEach(
-    (e) => (e.innerHTML = errorCapacityCodificationBits)
-  );
-  Array.from(document.querySelectorAll(CSS_CLASSES.MASK_CODIFICATION_BITS)).forEach((e) => (e.innerHTML = maskCodificationBits));
-  Array.from(document.querySelectorAll(CSS_CLASSES.FORMAT_INFORMATION_DATA_BITS)).forEach((e) => (e.innerHTML = formatDataInBinary));
-  Array.from(document.querySelectorAll(CSS_CLASSES.FORMAT_INFORMATION_CORRECTION_BITS)).forEach(
-    (e) => (e.innerHTML = formatCorrectionBits)
-  );
-  showHowFormatCorrectionBitsAreCalculated(formatDataInBinary);
-  document.querySelector(CSS_IDS.INFORMATION_FORMAT_15_BITS_AFTER_XOR).textContent = formatBitsAfterXor;
-  showFormatPatternCompletion(formatBitsAfterXor);
-
-  // convert message to ascii
-  ELEMENTS.ASCII_MESSAGE_TABLE.innerHTML = '';
-  for (let i = 0; i < message.length; i++) {
-    const div = document.createElement('div');
-    div.classList.add('element');
-
-    const letter = document.createElement('div');
-    letter.textContent = message[i];
-
-    const codification = document.createElement('div');
-    codification.textContent = `${message.charCodeAt(i)} = ${(+message.charCodeAt(i) >>> 0).toString(2).padStart(8, '0')}`;
-    codification.classList.add('monospace');
-
-    div.append(letter);
-    div.append(codification);
-    ELEMENTS.ASCII_MESSAGE_TABLE.append(div);
-  }
-
-  // Show how to enter data
-  const howToEnterDataExplanationMatrix = createQrCodeMatrix(qrCodeVersion.size);
-  paintModulesForAllPositionPatterns(howToEnterDataExplanationMatrix);
-  paintModulesForAllAlignmentPatterns(howToEnterDataExplanationMatrix);
-  paintModulesForAllTimingPatterns(howToEnterDataExplanationMatrix);
-  paintModulesForAllVersionPatterns(
-    howToEnterDataExplanationMatrix,
-    `${versionInBinary}${errorCorrectionBitsVersionPattern}`,
-    defaultDarkColor,
-    '#666'
-  );
-  paintModulesForAllFormatPatterns(howToEnterDataExplanationMatrix, formatBitsAfterXor);
-  // mode (data codification format, always binary in my case)
-  paintModulesForDataAndErrorRegion(howToEnterDataExplanationMatrix, '0100', modeDarkColor, modeLightColor);
-  // message length
-  paintModulesForDataAndErrorRegion(
-    howToEnterDataExplanationMatrix,
-    getMessageLengthAsBinary(qrCodeVersion.version, message.length),
-    numberOfCharactersEnteredDarkColor,
-    numberOfCharactersEnteredLightColor
-  );
-  // paint the data
-  paintModulesForDataAndErrorRegion(howToEnterDataExplanationMatrix, asciiToBinary(message), dataEnteredDarkColor, dataEnteredLightColor);
-  // padding bits (in my case always 4 modules)
-  paintModulesForDataAndErrorRegion(howToEnterDataExplanationMatrix, '0000');
-  // padding codewords
-  paintModulesForDataAndErrorRegion(
-    howToEnterDataExplanationMatrix,
-    getPaddingCodewords(qrCodeVersion, errorCapacity, message.length),
-    padCodewordDarkColor,
-    padCodewordLightColor
-  );
-  // error correction modules
-  paintModulesForDataAndErrorRegion(
-    howToEnterDataExplanationMatrix,
-    calculateErrorCorrectionBitsForDataEntered(message, qrCodeVersion, errorCapacity),
-    errorCodewordsDarkColor,
-    errorCodewordsLightColor
-  );
-  // reminder bits for qr code symbol (max 7 modules)
-  paintModulesForDataAndErrorRegion(howToEnterDataExplanationMatrix, '0000000', undefined, 'navajowhite');
-  paintSvgQrCode(CSS_IDS.HOW_TO_ENTER_DATA_EXPLANATION, howToEnterDataExplanationMatrix);
-
-  console.log(howToEnterDataExplanationMatrix);
-}
 
 // create the default qr code with the input placeholder
 const { message, errorCapacity } = readInputs();
-// generateQrCode(message, errorCapacity);
+generateQrCode(message, errorCapacity);
 
 //#endregion listener-create-qr-code
 
-function generateQrCode2(message, errorCorrectionLevel) {
+function generateQrCode(message, errorCorrectionLevel) {
   const QR_CODE_INFO_TO_USE = QR_CODE_INFO.find((qr) => qr.errorCorrectionLevel[errorCorrectionLevel].maxMessageLength >= message.length);
 
   if (QR_CODE_INFO_TO_USE === null) {
@@ -581,5 +443,3 @@ function generateQrCode2(message, errorCorrectionLevel) {
 
   window.scrollTo(0, document.body.scrollHeight);
 }
-
-generateQrCode2(message, errorCapacity);
--- src/how-to-enter-data-explanation.js
+++
@@ -1,6 +0,0 @@
-import { ELEMENTS } from './elements';
-
-export function showSvgHowToEnterDataExplanation(qrCodeVersion) {
-  const svg = ELEMENTS.HOW_TO_ENTER_DATA_EXPLANATION;
-  // TODO paint arrows
-}
